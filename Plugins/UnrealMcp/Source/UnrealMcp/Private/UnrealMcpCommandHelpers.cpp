b88b27bb7fe731363b8e7006bfd8328fe988965a
Boyang Meng <edwmeng@ucdavis.edu>
2026-05-04 22:17:00 +0900

    Trim MCP module entrypoint

---
+++ Plugins/UnrealMcp/Source/UnrealMcp/Private/UnrealMcpCommandHelpers.cpp
@@ -0,0 +1,20 @@
+#include "UnrealMcpModule.h"
+
+namespace UnrealMcp
+{
+	FString GetCommandRemainder(const FString& Input, const FString& Command)
+	{
+		if (Input.Len() <= Command.Len())
+		{
+			return FString();
+		}
+
+		return Input.Mid(Command.Len()).TrimStartAndEnd();
+	}
+
+	bool MatchesCommand(const FString& Input, const FString& Command)
+	{
+		return Input.Equals(Command, ESearchCase::IgnoreCase)
+			|| Input.StartsWith(Command + TEXT(" "), ESearchCase::IgnoreCase);
+	}
+}
